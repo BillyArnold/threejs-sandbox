744ed2bba69f1efb5a21a8474a60b0c4a89f8c6c
BillyArnold <billyarnoldd@gmail.com>
2024-04-18 15:35:13 +0100

    Update script.js ready for scroll lesson

--- script.js
+++ script.js
@@ -1,146 +1,90 @@
-import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import GUI from "lil-gui";
+import * as THREE from 'three'
+import GUI from 'lil-gui'
+
+/**
+ * Debug
+ */
+const gui = new GUI()
+
+const parameters = {
+    materialColor: '#ffeded'
+}
+
+gui
+    .addColor(parameters, 'materialColor')
 
 /**
  * Base
  */
-// Debug
-const gui = new GUI();
-
 // Canvas
-const canvas = document.querySelector("canvas.webgl");
+const canvas = document.querySelector('canvas.webgl')
 
 // Scene
-const scene = new THREE.Scene();
+const scene = new THREE.Scene()
 
 /**
- * Textures
+ * Test cube
  */
-const textureLoader = new THREE.TextureLoader();
-const particleTexture = textureLoader.load("./images/particles/2.png");
-
-const particlesGeometry = new THREE.BufferGeometry();
-const count = 5000;
-
-const positions = new Float32Array(count * 3);
-const colors = new Float32Array(count * 3); //rgb are the 3
-
-for (let i = 0; i < count * 3; i++) {
-  positions[i] = (Math.random() - 0.5) * 10;
-  colors[i] = Math.random();
-}
-
-const particlesMaterial = new THREE.PointsMaterial({
-  size: 0.15,
-  sizeAttenuation: true,
-  //color: "pink",
-  alphaMap: particleTexture,
-  transparent: true,
-  alphaTest: 0.001,
-  //depthWrite: false,
-  vertexColors: true,
-});
-
-particlesGeometry.setAttribute(
-  "position",
-  new THREE.BufferAttribute(positions, 3),
-);
-particlesGeometry.setAttribute("color", new THREE.BufferAttribute(colors, 3));
-
-//points
-const particles = new THREE.Points(particlesGeometry, particlesMaterial);
-scene.add(particles);
-
-/**
- * Lights
- */
-// Ambient light
-const ambientLight = new THREE.AmbientLight("#ffffff", 1);
-gui.add(ambientLight, "intensity").min(0).max(1).step(0.001);
-scene.add(ambientLight);
+const cube = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+)
+scene.add(cube)
 
 /**
  * Sizes
  */
 const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
-};
+    width: window.innerWidth,
+    height: window.innerHeight
+}
 
-window.addEventListener("resize", () => {
-  // Update sizes
-  sizes.width = window.innerWidth;
-  sizes.height = window.innerHeight;
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
 
-  // Update camera
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
 
-  // Update renderer
-  renderer.setSize(sizes.width, sizes.height);
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-});
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(
-  75,
-  sizes.width / sizes.height,
-  0.1,
-  100,
-);
-camera.position.x = 4;
-camera.position.y = 2;
-camera.position.z = 3;
-scene.add(camera);
-
-// Controls
-const controls = new OrbitControls(camera, canvas);
-controls.enableDamping = true;
+const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 6
+scene.add(camera)
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
-});
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-//renderer.setClearColor("#262837");
-//renderer.shadowMap.enabled = true;
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
  * Animate
  */
-const clock = new THREE.Clock();
+const clock = new THREE.Clock()
 
-const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
 
-  //particles.rotation.y = elapsedTime * 0.2;
-  for (let i = 0; i < count; i++) {
-    const i3 = i * 3;
+    // Render
+    renderer.render(scene, camera)
 
-    const x = particlesGeometry.attributes.position.array[i3];
-    const sinVal = x + elapsedTime;
-    //i3 gets the position x and + 1 gets the y, sin to make wave
-    particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(sinVal);
-    //this bad because of performance, should be done with specific shader
-  }
-  //particles needs to know whether to update if attributes are changed
-  //particlesGeometry.attributes.position.needsUpdate = true;
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
 
-  // Update controls
-  controls.update();
-
-  // Render
-  renderer.render(scene, camera);
-
-  // Call tick again on the next frame
-  window.requestAnimationFrame(tick);
-};
-
-tick();
+tick()
