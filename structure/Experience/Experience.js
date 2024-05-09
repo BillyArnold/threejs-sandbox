3509ba7d9004fecad8676cafb17e23145a7d43d4
Billy Arnold <billyarnoldd@gmail.com>
2024-05-09 20:33:57 +0100

    Starting to build into OOP

---
+++ structure/Experience/Experience.js
@@ -0,0 +1,34 @@
+import Sizes from "./Utils/Sizes.js";
+import Time from "./Utils/Time.js";
+import Camera from "./Camera.js";
+import * as THREE from "three";
+
+export default class Experience {
+  constructor(canvas) {
+    window.experience = this;
+
+    this.canvas = canvas;
+
+    this.sizes = new Sizes();
+    this.time = new Time();
+
+    this.scene = new THREE.Scene();
+    this.camera = new Camera();
+
+    this.sizes.on("resize", () => {
+      this.resize();
+    });
+
+    this.time.on("tick", () => {
+      this.update();
+    });
+  }
+
+  resize() {
+    console.log("resize");
+  }
+
+  update() {
+    console.log("update");
+  }
+}
